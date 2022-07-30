47550a640ea5aff57014e61de9f640d693e1de3c
Fernando de Lima <fernando.lima@ambevtech.com.br>
2022-07-30 18:24:16 -0300

    add rabbitmq spring configuration

---
+++ src/main/java/com/dpaula/notification/conf/RabbitMQConfig.java
@@ -0,0 +1,47 @@
+package com.dpaula.notification.conf;
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Fernando de Lima on 30/07/22
+ */
+@Configuration
+@AllArgsConstructor
+public class RabbitMQConfig {
+
+    private final ConnectionFactory connectionFactory;
+
+    /**
+     * Send messages to the queue
+     */
+    @Bean
+    public AmqpTemplate rabbitTemplate() {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter());
+        return rabbitTemplate;
+    }
+
+    /**
+     * Receive messages from the queue
+     */
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+        final var factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter jacksonConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
